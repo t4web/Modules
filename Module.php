c0a4a4e479530261787f1b870faf75e0622198a3
maxgu <maxgu@t4web.com.ua>
2014-09-21 10:47:19 +0300

    create Module.php module.config.php conposer.json tests env

---
+++ Module.php
@@ -0,0 +1,40 @@
+<?php
+namespace Modules;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ControllerProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface, ControllerProviderInterface,
+                        ServiceProviderInterface
+{
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array();
+    }
+
+    public function getControllerConfig()
+    {
+        return array();
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'modules' => __DIR__ . '/../view',
+        ),
+    ),
+
+);
